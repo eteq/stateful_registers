f4e27490cf49b93c5f2c2742c4eaf2b989441572
Erik Tollerud <erik.tollerud@gmail.com>
2018-08-16 23:46:02 -0400

    shells of concrete implementations

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from .register_state import *
+from .spi import *
+from .i2c import *
---
+++ i2c.py
@@ -0,0 +1,14 @@
+from .register_state import RegisterState
+
+__all__ = ['I2CRegisterState']
+
+class I2CRegisterState(RegisterState):
+    def __init__(self, registers, register_size):
+        super().__init__(registers, register_size)
+        raise NotImplementedError
+
+    def _read_register(self, address):
+        raise NotImplementedError
+
+    def _write_register(self, address, value):
+        raise NotImplementedError
