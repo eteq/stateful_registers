99a6821e1fc544e37780fcac8eaa4e2467341d59
Erik Tollerud <erik.tollerud@gmail.com>
2018-08-22 01:42:45 -0400

    implement i2c

--- stateful_registers/i2c.py
+++ stateful_registers/i2c.py
@@ -9,15 +9,26 @@ __all__ = ['I2CRegisterState']
 
 
 class I2CRegisterState(RegisterState):
-    def __init__(self, registers, register_size):
+    def __init__(self, registers, device_address, i2c_bus=1, register_size=8,
+             write_bit=7):
         if smbus is None:
             raise ImportError('smbus not present, cannot use I2CRegisterState')
-            
+
         super().__init__(registers, register_size)
-        raise NotImplementedError
+        self.bus = smbus.SMBus(i2c_bus)
+        self.device_address = device_address
+
+    @property
+    def device_address(self):
+        return self._device_address
+    @device_address.setter
+    def device_address(self, val):
+        if not isinstance(val, int):
+            raise TypeError('device_address must be an int')
+        self._device_address = val
 
     def _read_register(self, address):
-        raise NotImplementedError
+        return self.bus.read_byte_data(self._device_address, address)
 
     def _write_register(self, address, value):
-        raise NotImplementedError
+        self.bus.write_byte_data(self._device_address, address, value)
